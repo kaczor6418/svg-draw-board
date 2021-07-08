1a68b6fb25899bddf24e8602809a73c372946043
Krzysztof Kaczyński <k.kaczynski97@gmail.com>
2021-07-09 01:12:33 +0200

    feat: add reference element

--- SvgDrawer.js
+++ SvgDrawer.js
@@ -1,14 +1,21 @@
 export class SvgDrawBoard {
+    static referenceElmentColor = 'green';
+    static defaultElmentColor = 'black';
+
     #drawingArea;
+    #referenceElementProxy;
 
     constructor(drawingArea) {
         this.#drawingArea = drawingArea;
-        this.#referenceElement = null;
+        this.#referenceElementProxy = new Proxy({ circle: null }, {
+            set: this.#newReferenceElmentHandler
+        })
     }
 
-    drawCircle({x, y}, r = 10) {
-        const circle = this.#createSVGElement('circle', {cx: x.toString(), cy: y.toString(), r: r.toString()});
+    drawCircle({ x, y }, r = 10) {
+        const circle = this.#createSVGElement('circle', { cx: x.toString(), cy: y.toString(), r: r.toString(), fill: SvgDrawBoard.referenceElmentColor });
         this.#drawingArea.appendChild(circle);
+        this.#referenceElementProxy.circle = circle;
     }
 
     #createSVGElement(tag, attributes) {
@@ -17,6 +24,16 @@ export class SvgDrawBoard {
             svgElement.setAttribute(name, value);
         }
         return svgElement;
+    }
+
+    #newReferenceElmentHandler = (oldElement, prop, newElement) => {
+        if (prop in oldElement) {
+            oldElement[prop]?.setAttribute('fill', SvgDrawBoard.defaultElmentColor);
+            oldElement[prop] = newElement;
+            return true;
+        } else {
+            throw Error(`Property: ${prop.toString()} doesn't exists in reference elements`)
+        }
     }
 
 }
